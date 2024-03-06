f07b8e963102e2491ca9bb141a6f9245b0b649cb
Brian Jiménez Moedano <brianjmoedano@gmail.com>
2024-03-06 12:20:26 -0600

    Creates the XCode project and initial App config

---
+++ Grimm/Grimm/GrimmApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrimmApp.swift
+//  Grimm
+//
+//  Created by Brian Jiménez Moedano on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GrimmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
