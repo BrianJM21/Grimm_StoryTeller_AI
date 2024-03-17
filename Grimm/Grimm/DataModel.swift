77ab1f865d1a4b1447dd6bdfbcc9b16d92056114
Brian Jiménez Moedano <119526692+BrianJM21@users.noreply.github.com>
2024-03-17 00:13:34 -0600

    Se crea archivo DataModel

---
+++ Grimm/Grimm/DataModel.swift
@@ -0,0 +1,38 @@
+
+struct Tale {
+    let theme: String
+    let characters: [Character]
+    let place: String
+    let moral: String
+    let length: (type: Length, numberOf: Int)
+    let stage: Stage
+    }
+
+
+struct Character {
+    let name: String
+    let age: Int
+    let specie: String
+    let profession: String
+    let personality: String
+    let role: Role
+
+    enum Role: String {
+        case hero = "heroe"
+        case villain = "villano"
+        case neutral = "neutro"
+    }
+    }
+
+enum Length: String {
+    case words
+    case paragraphs
+    }
+
+enum Stage: String {
+    case baby = "bebé"
+    case preschooler = "preescolar"
+    case child = "infante"
+    case preteenager = "preadolescente"
+    case teenager = "adolescente"
+    }
